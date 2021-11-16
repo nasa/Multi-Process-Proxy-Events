16d650de7dccdb402ae203d4be3cb49b01d01c9c
Marshall, James C 459937851 <james.marshall-1@nasa.gov>
2021-11-16 09:55:22 -0500

    Add boilerplate legal comments

--- fsw/src/proxy_evs.h
+++ fsw/src/proxy_evs.h
@@ -1,7 +1,14 @@
-/*******************************************************************************
-** File: proxy_evs.h
+/*
+** GSC-18364-1, "Proxy Core Flight System Application and Client for External Process"
 **
-*******************************************************************************/
+** Copyright © 2019 United States Government as represented by
+** the Administrator of the National Aeronautics and Space Administration.
+** No copyright is claimed in the United States under Title 17, U.S. Code.
+** All Other Rights Reserved.
+**
+** Licensed under the NASA Open Source Agreement version 1.3
+** See "NOSA GSC-18364-1.pdf"
+*/
 
 #ifndef _proxy_evs_h_
 #define _proxy_evs_h_
